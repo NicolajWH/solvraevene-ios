b1ffafb6e77f667f1c48b8eba50f5f25a19cf918
Nicolaj Worsaae Haugaard <nicolaj@worsa.ae>
2026-05-04 01:01:15 +0200

    Add coordinate property to Trip struct

--- Solvraevene/Models/Trip.swift
+++ Solvraevene/Models/Trip.swift
@@ -1,10 +1,22 @@
 import Foundation
+import CoreLocation
 
 struct Trip: Identifiable, Codable {
     var id = UUID()
     let date: String
     let location: String
     let organizers: [String]
+
+    var coordinate: CLLocationCoordinate2D? {
+        switch location {
+        case "Wolfsburg":
+            return CLLocationCoordinate2D(latitude: 52.4227, longitude: 10.7865)
+        case "Henne Kirkeby":
+            return CLLocationCoordinate2D(latitude: 55.7300, longitude: 8.2200)
+        default:
+            return nil
+        }
+    }
 
     enum CodingKeys: String, CodingKey {
         case date
